2d95b7b3303827eb31705e7cb66c5f2e20405be4
Lucas <lucas.thullier@outlook.fr>
2021-04-26 18:00:56 +0200

    delete useless line

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const fs = require("fs");
-const auth = require("./middlewares/auth");
 const path = require("path");
 
 const app = express();
